8d47e93d967bff6ad219680d62f0419d84e010fa
That is me <trab.die@gmail.com>
2021-12-02 19:46:10 -0500

    add

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,5 +1,6 @@
 
 
+import java.lang.Math;
 
 public class NumberUtilities {
 
@@ -7,8 +8,8 @@ public class NumberUtilities {
         // initialise the string
         String string = "";
         // a loop to return all integers way up to the stop but not include the stop
-        for (int k = 0; k < stop; k++) {
-            string =string +k;
+        for (int num = 0; num < stop; num++) {
+            string =string + num;
         }
         return string;
     }
@@ -18,8 +19,8 @@ public class NumberUtilities {
         // initialise the string
         String string = "";
         // a loop to return all integers way up to the stop but not include the stop
-        for (int k = start; k < stop; k++) {
-            string =string +k;
+        for (int num= start; num < stop; num++) {
+            string =string +num;
         }
         return string;
 
@@ -32,23 +33,56 @@ public class NumberUtilities {
         // initialise the string
         String string = "";
         // a loop to return all integers way up to the stop but not include the stop
-        for (int k = 0; k < stop; k+=step) {
-            string =string +k;
+        for (int num = start; num < stop; num+=step) {
+            string =string +num;
         }
         return string;
     }
 
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        // initialise the string
+        String string = "";
+        // a loop to return all integers way up to the stop but not include the stop
+        for (int num = start; num < stop; num++) {
+            if ( num %2 == 0) {
+                string =string +num;
+            }
+
+        }
+        return string;
+
     }
+
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        // initialise the string
+        String string = "";
+        // a loop to return all integers way up to the stop but not include the stop
+        for (int num = start; num < stop; num++) {
+            if ( num %2 != 0) {
+                string =string +num;
+            }
+        }
+        return string;
+
+
     }
 
 
+
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+
+        // initialise the string
+        String string = "";
+        // a loop to return all integers way up to the stop but not include the stop
+        for (int num = start; num < stop; num++) {
+                int j = (int) (Math.pow(num, exponent));
+                string += (int)j ;
+
+        }
+        return string;
+
     }
 }
