9afb19843864198a237f355c2be4e940f50ff8e2
That is me <trab.die@gmail.com>
2021-12-02 23:42:11 -0500

    update table multip

--- TableUtilities.java
+++ TableUtilities.java
@@ -4,16 +4,29 @@ public class TableUtilities {
     public static String getSmallMultiplicationTable() {
 
 
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
 
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
 
-        return null;
+        String outputTable = "";
+        // iteration through row
+        for ( int i=1 ; i<= tableSize; i++) {
+            // iteration through column
+            for ( int j=1 ; j<= tableSize; j++){
+
+                // allocate three spaces to the result of i*j followed by a space and |
+                outputTable +=outputTable.format("%3d |",(i*j));
+            }
+            // new line after each row and column
+            outputTable+="\n";
+        }
+
+        return outputTable;
     }
 }
